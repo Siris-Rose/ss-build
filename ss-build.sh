95a7c29a5e49ed8b8ca7c9737bf64835115310f1
Siris-Rose <51070794+Siris-Rose@users.noreply.github.com>
2020-02-01 14:18:42 +0800

    Update ss-build.sh

--- ss-build.sh
+++ ss-build.sh
@@ -1,6 +1,8 @@
 ﻿#! /bin/bash
-# Copyright (c) 2019 Siris-Rosered='\033[0;31m'
+# Copyright (c) 2019 Siris-Rose
 # Special thanks to flyzy
+
+red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
